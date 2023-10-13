fb362aa0c6153ff1cad9f32d19f81fe24ea06b15
Guillaume Le Saux <guillaume.le_saux@edu.devinci.fr>
2023-10-13 12:43:23 +0200

    ajout des commentaires

--- main.js
+++ main.js
@@ -1,13 +1,16 @@
+// initialisation
 let equipemancheAttaque = 0;
 let equipemancheDefense = 0;
 
 let joueurs = ["Cypher", "Omen", "Phoenix", "Astra", "Killjoy"];
 let equipes = ["Attaquant", "Defenseur"];
 
+//Tant que des deux equipes n'a pas 13 manches gagnées
 while (equipemancheAttaque < 13 && equipemancheDefense < 13) {
   let equipeAttaquanteJoueurs = Array.from(joueurs);
   let equipeDefenseJoueurs = Array.from(joueurs);
 
+  // condition de victoire et de défaite
   while (
     equipeAttaquanteJoueurs.length > 0 &&
     equipeDefenseJoueurs.length > 0
@@ -25,6 +28,7 @@ while (equipemancheAttaque < 13 && equipemancheDefense < 13) {
     let joueurMort = "";
     let action = "";
 
+    // si le joueur mort est défenseur : 60% de chance d’amorcer le spike,si le joueur mort est attaquant, 40% de chance.
     if (Math.random() < 0.6) {
       joueurMort = "défenseur";
       action = "amorce le spike";
@@ -33,9 +37,11 @@ while (equipemancheAttaque < 13 && equipemancheDefense < 13) {
       action = "tue son adversaire";
     }
 
+    // si le spike est amorcé, 70% de chance que lors d’un duel, l’attaquant l’emporte sur le défenseur, donc 30% pour le défenseur de tuer.
     if (Math.random() < 0.7 && action.includes("amorce le spike")) {
       action += ", l'attaquant l'emporte sur le défenseur";
-    } else {
+    } //si le spike n’est pas amorcé, 50% de chance pour chaque duel.
+    else {
       action += ", 50/50 duel nul";
     }
 
@@ -53,7 +59,7 @@ while (equipemancheAttaque < 13 && equipemancheDefense < 13) {
       );
     }
   }
-
+  // Une manche est terminée lorsqu’il ne reste plus que des joueurs d’une même équipe.Condition d'élimination d'une des deux equipes
   if (equipeAttaquanteJoueurs.length === 0) {
     console.log(
       "L'équipe des attaquants a été éliminée. Les défenseurs gagnent cette manche !"
@@ -67,6 +73,7 @@ while (equipemancheAttaque < 13 && equipemancheDefense < 13) {
   }
 }
 
+// Insertion du score final
 if (equipemancheAttaque === 13) {
   console.log(
     "L'équipe des attaquants a remporté 13 manches et et a marbré les défenseurs !"
@@ -80,41 +87,3 @@ if (equipemancheAttaque === 13) {
 }
 
 console.log("Fin de game !");
-
-// for (let i = 0; i <= 2; i++) {
-//   let nom = equipe[Math.floor(Math.random() * equipe.length)];
-//   let typEquipe = typEquipes[Math.floor(Math.random() * typEquipes.length)];
-//   let joueur = Object.create(Joueur);
-//   joueur.init(nom, typEquipe);
-//   joueurs.push(joueur);
-// }
-
-// console.log(`${joueur.nom}`);
-// let rand1 = Math.floor(Math.random() * equipe1.length);
-// let rjoueur1 = equipe1[rand1];
-// console.log("joueur de l'equipe 1: " + rjoueur1);
-
-// let equipe2 = ["Cypher", "Omen", "Phoenix", "Astra", "Killjoy"];
-// let rand2 = Math.floor(Math.random() * equipe2.length); // un peu degueu de faire ça mais bon
-// let rand3 = Math.floor(Math.random() * equipe2.length);
-// let rjoueur2 = equipe2[rand3];
-// console.log("jouer de l'equipe 2: " + rjoueur2);
-
-// equipe1 = "Defenseur";
-// equipe2 = "Attaquant";
-
-// while (equipe1manche <= 13 || equipe2manche <= 13) {
-//   equipe1manche++;
-//   // let equipes = [equipe1, equipe2];
-//   // let rand = Math.floor(Math.random() * equipes.length);
-//   // console.log(rjoueur1 + "des" + equipes + "s'en va pour marbrer" + rjoueur2);
-
-//   let rValue = equipes[rand];
-//   console.log(rValue);
-//   if ((rValue = "Defenseur")) {
-//     chance = 0.6;
-//   } else if ((rValue = "Attaquant")) {
-//     chance = 0.4;
-//   }
-//   console.log(chance);
-// }
